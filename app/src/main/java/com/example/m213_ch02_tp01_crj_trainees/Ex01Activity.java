06406bf8dfe420babd11a8c03257794ef0cabfc7
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-03-04 10:26:52 +0000

    Initial commit

---
+++ app/src/main/java/com/example/m213_ch02_tp01_crj_trainees/Ex01Activity.java
@@ -0,0 +1,14 @@
+package com.example.m213_ch02_tp01_crj_trainees;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ex01Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex01);
+    }
+}
